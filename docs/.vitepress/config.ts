622361875bf1d06dbb782db05ccc16c5d808f6c2
David Simpson <david.simpson@vastare.co.uk>
2025-11-15 16:15:09 +0000

    Started revamping NPM build command structure.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  title: 'Foundry',
-  description: 'Vastare Design Foundation',
-  base: '/foundry/',
+  title: 'Foundry Documentation',
+  description: 'Vastare\'s Design Foundation',
+  outDir: "../public",
   themeConfig: {
+    logo: {
+      light: "/img/symbol-blue.png",
+      dark: "/img/symbol-white.png"
+    },
     nav: [
       { text: 'Getting Started', link: '/getting-started' },
       { text: 'Usage', link: '/usage/' },
       { text: 'API', link: '/reference/' }
-    ]
+    ],
+    sidebar: {
+      '/getting-started/': [
+        {
+          text: "Introduction",
+          items: [
+            { text: "What is Foundry?", link: "/usage/" }
+          ]
+        }
+      ]
+    }
   }
 });
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,4 @@
+import DefaultTheme from "vitepress/theme";
+import '../../dist/styles/main.scss';
+
+
